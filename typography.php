9c6ada5e05eae018bdea044d22bb42fc5daa84fd
James Fleeting <hello@jamesfleeting.com>
2014-09-10 15:14:21 -0500

    Adding our icon font, setting icon variables, and styling tweaks.

--- typography.php
+++ typography.php
@@ -1,13 +1,17 @@
 <?php include('inc_header.php'); ?>
 
   <div class="row">
-    <div class="full">
-      <h1>HTML Ipsum Presents</h1>
+    <div class="page-title">
+      <h1>H1 Page Title</h1>
       <p class="subtitle">Sub title we are the monkees...</p>
+    </div>
+  </div>
 
-      <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-
+  <div class="row">
+    <div class="full">
       <p class="intro">Intro paragraph donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
+
+      <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
 
       <h3>Buttons</h3>
 
@@ -22,7 +26,15 @@
         <li>Aliquam tincidunt mauris eu risus.</li>
       </ol>
 
-      <blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
+      <div class="row">
+        <div class="half">
+          <img src="http://www.fillmurray.com/g/750/750" alt="">
+        </div>
+
+        <div class="half">
+          <blockquote>&ldquo;I go home and stay there. I wash and scrub up each day, and that's it. One month I actually grew a moustache, just so I could say that I'd done something.&rdquo;<cite>- Bill Murray</cite></blockquote>
+        </div>
+      </div>
 
       <h3>Header Level 3</h3>
 
