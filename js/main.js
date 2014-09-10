25bf4359f0683991d0442b91746656eb69cdd7a4
smhigley <sarahmhigley@gmail.com>
2014-09-10 16:14:27 -0500

    troop accordion js and styles

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 (function($) {
 
+  // troop dropdowns
+  $('.troop-list').on('click', '.trigger', function(e) {
+    e.preventDefault();
+
+    var $this = $(this),
+        $li = $this.parent(),
+        $troop_list = $this.parents('.troop-list'),
+        $dropdown = $this.siblings('.profile-content');
+
+    // if closing the accordion, change classes and be done
+    if ($li.hasClass('active')) {
+      $li.removeClass('active');
+      $troop_list.children('li').removeClass('inactive');
+      return;
+    }
+
+    $troop_list.children('li').not($li).removeClass('active').addClass('inactive');
+    $li.addClass('active');
+
+    // set dropdown top to correct value
+    var offset_top = $li.position().top + $li.outerHeight();
+    $dropdown.css('top', offset_top + 'px');
+
+  });
+
 }(jQuery));
