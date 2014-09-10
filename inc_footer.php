f81ca751981001b470d07bacf03f8660f91df512
James Fleeting <hello@jamesfleeting.com>
2014-09-10 11:03:03 -0500

    Moving to PHP includes.

---
+++ inc_footer.php
@@ -0,0 +1,33 @@
+<footer role="contentinfo">
+  <div class="row">
+    <ul class="footer-social">
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+      <li><a href="#" title=""><img src="/images/temp-footer-social-icon.png" alt=""></a></li>
+    </ul>
+
+    <div class="bananas"> <!-- TODO: This should be a black triangle behind the bananas. -->
+      <img src="/images/bananas.png" alt="Nom Nom Nom">
+    </div>
+
+    <p class="footer-meta"><strong>&copy; Monkee-Boy Web Design, Inc. 2014</strong> // <a href="#" title="Privacy">Privacy</a><br />
+      9390 Research Blvd. Suite 425 Austin, TX  78759 // 512-335-2221</p>
+  </div>
+</footer>
+
+<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+<script src="/js/app.min.js"></script>
+<script>
+(function(m,o,n,k,e,y){m.GoogleAnalyticsObject=k;m[k]||(m[k]=
+function(){(m[k].q=m[k].q||[]).push(arguments)});m[k].k=+new Date;
+e=o.createElement(n);y=o.getElementsByTagName(n)[0];
+e.src='//www.google-analytics.com/analytics.js';
+y.parentNode.insertBefore(e,y)}(window,document,'script','ga'));
+ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+</body>
+</html>
