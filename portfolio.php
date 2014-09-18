657fe9e5b965cdcb3cdb3a604556d87b13f88974
smhigley <sarahmhigley@gmail.com>
2014-09-18 09:19:28 -0500

    portfolio service section

--- portfolio.php
+++ portfolio.php
@@ -10,42 +10,56 @@
 
   <div class="row">
 
-    <div class="port-slider">
+    <div class="port-slider" data-site="balcones">
       <div class="screens initial">
         <div class="desktop screen active" data-device="desktop">
           <div class="screen-inner">
-            <img src="images/temp-port-screen1.jpg">
+            <img src="/assets/port-balcones-desktop-home.png">
           </div>
         </div>
-        <div class="tablet screen next" data-device="tablet">
+        <div class="tablet screen right" data-device="tablet">
           <div class="screen-inner">
-            <img src="images/temp-port-screen1-tablet.jpg">
+            <img src="/assets/port-balcones-tablet-home.png">
           </div>
         </div>
-        <div class="phone screen prev" data-device="phone">
+        <div class="phone screen left" data-device="phone">
           <div class="screen-inner">
-            <img src="images/temp-port-screen1-phone.jpg">
+            <img src="/assets/port-balcones-phone-home.png">
           </div>
         </div>
+        <div class="slider-nav">
+          <a href="#" class="port-left">Rotate Left</a>
+          <a href="#" class="port-right">Rotate Right</a>
+        </div><!-- /.slider-nav -->
       </div><!-- /.screens -->
-      <div class="slider-nav">
-        <a href="#" class="port-next">Next</a>
-        <a href="#" class="port-prev">Previous</a>
-      </div>
       <div class="thumbs">
         <span class="instructions">please pick a screen to the right to view in the device above</span>
-        <a href="#" class="thumbnail active">
+        <a href="#" class="thumbnail active" data-screen="home">
           <img src="/images/temp-port-screen1.jpg" alt="Home Page">
         </a>
-        <a href="#" class="thumbnail">
+        <a href="#" class="thumbnail" data-screen="sub1">
           <img src="/images/temp-port-screen2.jpg" alt="Sub Page 1">
         </a>
-        <a href="#" class="thumbnail">
+        <a href="#" class="thumbnail" data-screen="sub2">
           <img src="/images/temp-port-screen3.jpg" alt="Sub Page 2">
         </a>
       </div>
     </div><!-- /.port-slider -->
 
+  </div>
+
+  <div class="row">
+    <div class="full">
+      <h2>Services Provided</h2>
+      <ul class="service-icons">
+        <li><span class="strategy service-icon"><i></i></span>Research &amp; Strategy</li>
+        <li><span class="design service-icon"><i></i></span>Design</li>
+        <li><span class="development service-icon"><i></i></span>Development</li>
+        <li><span class="marketing service-icon"><i></i></span>Marketing</li>
+        <li><span class="maintenance service-icon"><i></i></span>Maintenance</li>
+        <li><span class="growth service-icon"><i></i></span>Growth</li>
+      </ul>
+    </div>
   </div>
 
 <?php include('inc_footer.php'); ?>
