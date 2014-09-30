f9d2f3817db5f8c259d88cdced9315cee7b8e7fc
James Fleeting <hello@jamesfleeting.com>
2014-09-30 13:34:35 -0500

    Adding featured clients and pulse to the homepage.

--- index.php
+++ index.php
@@ -54,19 +54,73 @@
 
   <div class="panel-wide">
     <div class="row-flush">
-test
+      Marketing Section
     </div>
   </div>
 
-  <div class="panel-wide" data-panel-color="teal" data-panel-style="slash">
+  <div class="panel-wide home-clients" data-panel-color="green" data-panel-style="slash">
+    <div class="row section-head" data-text-align="right">
+      <span class="section-title dots" data-dots="left"><em>a few of our</em> lovely clients</span>
+    </div>
+
     <div class="row-flush">
-test
+      <ul class="featured-clients styleless">
+        <li><a href="" title=""><img src="/assets/client-bullock.png" alt=""></a></li>
+        <li><a href="" title=""><img src="/assets/client-austinchamber.png" alt=""></a></li>
+        <li><a href="" title=""><img src="/assets/client-bullock.png" alt=""></a></li>
+        <li><a href="" title=""><img src="/assets/client-austinchamber.png" alt=""></a></li>
+      </ul>
     </div>
   </div>
 
-  <div class="panel-wide" data-panel-color="green">
+  <div class="panel-wide home-pulse" data-panel-color="teal">
+    <div class="row section-head" data-text-align="left">
+      <span class="section-title dots" data-dots="right"><em>the</em><br />pulse</span>
+    </div>
+
     <div class="row-flush">
-test
+      <div class="one-third item-panel">
+        <h4>Latest News</h4>
+
+        <div class="item-panel-inside" data-text-align="center">
+          <figure><img src="/assets/news-icon.png" alt=""></figure>
+
+          <p><a href="#" title="">Former Superhero joins team Monkee-Boy »</a></p>
+          <time>09-03</time>
+        </div>
+      </div>
+
+      <div class="one-third item-panel alt">
+        <h4>Sign Up For Our Newsletter</h4>
+
+        <div class="item-panel-inside" data-text-align="center">
+          <form class="form-newsletter" action="" method="post">
+            <div class="subscribe-status success"></div>
+
+            <label for="form-email">Enter your email</label>
+            <input type="email" name="email" id="form-email">
+            <input type="submit" value="Subscribe!">
+          </form>
+        </div>
+      </div>
+
+      <div class="one-third item-panel">
+        <h4>On Twitter</h4>
+
+        <div class="item-panel-inside">
+          [insert twitter widget]
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="panel-wide home-blog" data-panel-color="light-grey">
+    <div class="row section-head" data-text-align="center">
+      <span class="section-title dots" data-dots="dual">from the blog</span>
+    </div>
+
+    <div class="row-flush">
+      [lorem ipsum]
     </div>
   </div>
 
