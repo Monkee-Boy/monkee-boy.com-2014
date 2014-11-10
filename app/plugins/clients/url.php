7758f008e1f26363b3915087103a5abaa755d9cc
John Hoover <defvayne23@gmail.com>
2014-11-10 14:26:36 -0600

    Add back clients plugin

---
+++ app/plugins/clients/url.php
@@ -0,0 +1,44 @@
+<?php
+# Custom URL using mod_rewrite
+
+### Url Pattern ###############################
+/*
+ # Function Variable Order:
+ #   1. URL parameters (<name:[a-z]+>)
+ #   2. Pattern parameters
+ #
+ # Example URL Patterns:
+ #   /page/<name:[a-z0-9]+>/
+ #   /<tag:[a-z]+>/
+*/
+$aPluginUrlPatterns = array(
+  "/admin/clients/" => array(
+    "cmd" => "admin_clients",
+    "action" => "index"
+  ),
+  "/admin/clients/add/" => array(
+    "cmd" => "admin_clients",
+    "action" => "add"
+  ),
+  "/admin/clients/add/s/" => array(
+    "cmd" => "admin_clients",
+    "action" => "add_s"
+  ),
+  "/admin/clients/edit/<id:[0-9]+>/" => array(
+    "cmd" => "admin_clients",
+    "action" => "edit"
+  ),
+  "/admin/clients/edit/s/" => array(
+    "cmd" => "admin_clients",
+    "action" => "edit_s"
+  ),
+  "/admin/clients/delete/<id:[0-9]+>/" => array(
+    "cmd" => "admin_clients",
+    "action" => "delete"
+  ),
+  "/admin/clients/sort/<id:[0-9]+>/<sort:[a-z]+>/" => array(
+    "cmd" => "admin_clients",
+    "action" => "sort"
+  ),
+);
+###############################################
