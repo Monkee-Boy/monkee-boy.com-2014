c9efc9e4a85d725ecb1bf918424b73cc088695fd
John Hoover <defvayne23@gmail.com>
2014-12-15 17:33:34 -0600

    Change news admin to use post date instead of created date

--- app/plugins/news/model.php
+++ app/plugins/news/model.php
@@ -108,15 +108,8 @@ class news_model extends appModel {
 				$aArticle["excerpt"] = (string)substr(nl2br(htmlspecialchars(stripslashes(strip_tags($aArticle["content"])))), 0, $this->excerptCharacters);
 
 			$aArticle["content"] = stripslashes($aArticle["content"]);
-			$aArticle["url"] = "/news/".date("Y", $aArticle["created_datetime"])."/".date("m", $aArticle["created_datetime"])."/".date("d", $aArticle["created_datetime"])."/".$aArticle["tag"]."/";
+			$aArticle["url"] = "/news/".date("Y", $aArticle["publish_on"])."/".date("m", $aArticle["publish_on"])."/".date("d", $aArticle["publish_on"])."/".$aArticle["tag"]."/";
 			$aArticle["publish_on"] = strtotime($aArticle["publish_on"]);
-
-			$aArticle["author"] = $this->dbQuery(
-				"SELECT * FROM `{dbPrefix}users`"
-					." WHERE `id` = ".$this->dbQuote($aArticle["authorid"], "integer")
-					." LIMIT 1"
-				,"row"
-			);
 
 			$aArticle["categories"] = $this->dbQuery(
 				"SELECT * FROM `{dbPrefix}news_categories` AS `categories`"
--- app/plugins/news/views/admin/edit.php
+++ app/plugins/news/views/admin/edit.php
@@ -4,7 +4,7 @@
 	<?php $this->tplDisplay('inc_alerts.php'); ?>
 
 	<form id="edit-form" method="post" action="/admin/news/edit/s/" enctype="multipart/form-data">
-		<input type="hidden" name="id" value="<?= $aPost['id'] ?>">
+		<input type="hidden" name="id" value="<?= $aArticle['id'] ?>">
 		<div class="row-fluid">
 			<div class="span8">
 				<div class="accordion-group">
@@ -14,7 +14,7 @@
 					<div id="pagecontent" class="accordion-body">
 						<div class="accordion-inner">
 							<div class="controls">
-								<input type="text" name="title" id="form-title" value="<?= $aPost['title'] ?>" class="span12 validate[required]">
+								<input type="text" name="title" id="form-title" value="<?= $aArticle['title'] ?>" class="span12 validate[required]">
 							</div>
 						</div>
 					</div>
@@ -27,7 +27,7 @@
 					<div class="accordion-body">
 						<div class="accordion-inner">
 							<div class="controls">
-								<?= html_editor($aPost['content'], "content") ?>
+								<?= html_editor($aArticle['content'], "content") ?>
 							</div>
 						</div>
 					</div>
@@ -40,7 +40,7 @@
 					<div class="accordion-body">
 						<div class="accordion-inner">
 							<div class="controls">
-								<textarea name="excerpt" class="span12" style="height:115px;"><?= $aPost['excerpt'] ?></textarea>
+								<textarea name="excerpt" class="span12" style="height:115px;"><?= $aArticle['excerpt'] ?></textarea>
 								<p class="help-block"><span id="currentCharacters"></span> of <?= $sExcerptCharacters ?> characters</p>
 							</div>
 						</div>
@@ -58,7 +58,7 @@
 								<?php if(!empty($aCategories)): ?>
 									<select name="categories[]" data-placeholder="Select Categories" class="chzn-select span12" multiple="">
 										<?php foreach($aCategories as $aCategory): ?>
-											<option value="<?= $aCategory['id'] ?>"<?php if(in_array($aCategory['id'], $aPost['categories'])){ echo ' selected="selected"'; } ?>><?= $aCategory['name'] ?></option>
+											<option value="<?= $aCategory['id'] ?>"<?php if(in_array($aCategory['id'], $aArticle['categories'])){ echo ' selected="selected"'; } ?>><?= $aCategory['name'] ?></option>
 										<?php endforeach; ?>
 				          </select>
 
@@ -82,7 +82,7 @@
 						<div class="accordion-inner">
 							<div class="control-group cf">
 								<div class="controls">
-									<?php if($aPost['active'] != 1): ?>
+									<?php if($aArticle['active'] != 1): ?>
 										<input type="submit" name="submit-type" value="Save Draft" class="btn pull-left">
 										<input type="submit" name="submit-type" value="Publish" class="btn btn-primary pull-right">
 									<?php else: ?>
@@ -93,7 +93,7 @@
 
 							<div class="control-group">
 								<div class="controls">
-									<?php if($aPost['active'] == 1): ?><label class="checkbox"><input type="checkbox" name="active" id="form-active" value="1"<?php if($aPost['active'] == 1){ echo ' checked="checked"'; } ?>>Publish post to the website.</label><?php endif; ?>
+									<?php if($aArticle['active'] == 1): ?><label class="checkbox"><input type="checkbox" name="active" id="form-active" value="1"<?php if($aArticle['active'] == 1){ echo ' checked="checked"'; } ?>>Publish post to the website.</label><?php endif; ?>
 								</div>
 							</div>
 						</div>
@@ -102,16 +102,16 @@
 
 				<div class="accordion-group">
 					<div class="accordion-heading">
-						<span class="accordion-toggle">Schedule</span>
+						<span class="accordion-toggle">Publish(ed) On</span>
 					</div>
 					<div class="accordion-body">
 						<div class="accordion-inner">
 							<div class="control-group">
 								<div class="controls timepicker">
-									<input type="input" name="publish_on_date" value="<?= $aPost['publish_on_date'] ?>" id="datepicker" class="span12">
-									@ <?= html_select_time($aPost['publish_on'], "publish_on_", 15, false, false); ?>
+									<input type="input" name="publish_on_date" value="<?= $aArticle['publish_on_date'] ?>" id="datepicker" class="span12">
+									@ <?= html_select_time($aArticle['publish_on'], "publish_on_", 15, false, false); ?>
 
-									<p class="help-block">The post will be pending until this date and time then it will automatically publish.</p>
+									<p class="help-block">If the date is in the future, it will not show on the site till then.</p>
 								</div>
 							</div>
 						</div>
@@ -126,11 +126,11 @@
 						<div class="accordion-inner">
 							<div class="control-group">
 								<div class="controls">
-									<label class="checkbox"><input type="checkbox" name="post_twitter" value="1"<?php if($aPost['post_twitter'] == 1){ echo' checked="checked"'; } ?>> <img src="/images/admin/social/twitter.png" width="15px"> Share this post to Twitter.</label>
+									<label class="checkbox"><input type="checkbox" name="post_twitter" value="1"<?php if($aArticle['post_twitter'] == 1){ echo' checked="checked"'; } ?>> <img src="/images/admin/social/twitter.png" width="15px"> Share this post to Twitter.</label>
 								</div>
 
 								<div class="controls">
-									<label class="checkbox"><input type="checkbox" name="post_facebook" value="1"<?php if($aPost['post_facebook'] == 1){ echo ' checked="checked"'; } ?>> <img src="/images/admin/social/facebook_32.png" width="15px"> Share this post to Facebook.</label>
+									<label class="checkbox"><input type="checkbox" name="post_facebook" value="1"<?php if($aArticle['post_facebook'] == 1){ echo ' checked="checked"'; } ?>> <img src="/images/admin/social/facebook_32.png" width="15px"> Share this post to Facebook.</label>
 								</div>
 							</div>
 						</div>
@@ -144,7 +144,7 @@
 					<div class="accordion-body in collapse">
 						<div class="accordion-inner">
 							<div class="controls">
-								<textarea name="tags" id="form-tags" style="height:115px;" class="span12"><?= $aPost['tags'] ?></textarea>
+								<textarea name="tags" id="form-tags" style="height:115px;" class="span12"><?= $aArticle['tags'] ?></textarea>
 								<p class="help-block">Comma separated list of keywords.</p>
 							</div>
 						</div>
