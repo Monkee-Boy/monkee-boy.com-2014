a0ef5242d43cb17110c789ac7e7bfa9cc9affed6
John Hoover <defvayne23@gmail.com>
2014-12-15 13:07:49 -0600

    Style 404 page

--- app/views/error/404.php
+++ app/views/error/404.php
@@ -1,6 +1,16 @@
 <?php $this->tplDisplay('inc_header.php', ['menu'=>'404','sPageTitle'=>'Page Not Found']); ?>
 
-<h2>Page Not Found</h2>
-<p><strong>Error 404</strong>: The page you're looking for has been moved or no longer exists. You can start back at our <a href="/" title="Home Page">home page</a> or <a href="/search/" title="Search Site">search</a> for the page your looking for.</p>
+  <div class="row">
+    <div class="page-title">
+      <h1>Page Not Found</h1>
+      <p class="subtitle">Error 404</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="full">
+      <p><strong>Error 404</strong>: The page you're looking for has been moved or no longer exists. You can start back at our <a href="/" title="Home Page">home page</a> or <a href="/search/" title="Search Site">search</a> for the page your looking for.</p>
+    </div>
+  </div>
 
 <?php $this->tplDisplay('inc_footer.php'); ?>
