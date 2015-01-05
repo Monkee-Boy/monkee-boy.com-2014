b80bd10bc36699b2f9c21875dafe675263d68dc3
John Hoover <defvayne23@gmail.com>
2015-01-05 10:54:33 -0600

    Show subtitle for content pages.

--- app/views/content.php
+++ app/views/content.php
@@ -1,9 +1,8 @@
 <?php $this->tplDisplay("inc_header.php", ['menu'=>$aContent['tag'],'sPageTitle'=>$aContent['title']]); ?>
 
-	<div class="row">
-		<div class="page-title">
-			<h1><?php echo $aContent['title'] ?></h1>
-		</div>
+	<div class="row page-title">
+		<h1><?php echo $aContent['title'] ?></h1>
+		<span class="subtitle"><?php echo $aContent['subtitle'] ?></span>
 	</div>
 
 	<div class="row">
