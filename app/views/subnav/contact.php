16ee1a4385cfb4428c95cc56c1dc16932fdc898e
John Hoover <defvayne23@gmail.com>
2015-01-06 16:48:08 -0600

    Add sub-nav for Find Us and Work With Us.

---
+++ app/views/subnav/contact.php
@@ -0,0 +1,4 @@
+<ul class="columns-2">
+  <li><a href="/contact/" title="" class="nav-contact"<?php if($menu === 'contact'): ?> class="current"<?php endif; ?>>Find Us</a></li>
+  <li><a href="/contact/work-with-us/" title="Work With Us"<?php if($menu === 'work-with-us'): ?> class="current"<?php endif; ?>>Work With Us</a></li>
+</ul>
