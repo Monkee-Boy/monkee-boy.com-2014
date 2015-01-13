abf5440759b96bbe78d324988824696fe4511279
James Fleeting <twofivethreetwo@gmail.com>
2015-01-13 11:29:43 -0600

    Pulling content from the admin for thank you page. [deploy: dev]

--- app/plugins/quote/views/thank_you.php
+++ app/plugins/quote/views/thank_you.php
@@ -26,10 +26,7 @@ $this->tplDisplay("inc_header.php", ['menu'=>'work-with-us', 'sPageTitle'=>$sTit
 
   <div class="row">
     <div class="single-column content-block">
-      <h2>thanks!</h2>
-      <h3>Your Form has been submitted. we can’t wait to Look it over.</h3>
-      <p>We’ll be in touch very soon. In the meantime here’s a picture of our fake office cat. If you want to help us name him feel free to <a href="https://twitter.com/monkeeboy">send a tweet</a> our way with your recommendation.</p>
-      <img src="/images/thankyou-cat.png" alt="our office kitty says thank you">
+      <?= $aContent['content']; ?>
     </div>
   </div>
 
