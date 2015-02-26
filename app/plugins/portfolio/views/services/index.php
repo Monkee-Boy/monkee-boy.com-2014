6997cb02e37bd14dd5124b9ab1f6ea26011a0bf6
James Fleeting <hello@jamesfleeting.com>
2015-02-26 09:57:04 -0600

    Styling tweak to panel header links and adding CTA to services.

--- app/plugins/portfolio/views/services/index.php
+++ app/plugins/portfolio/views/services/index.php
@@ -23,4 +23,17 @@
   </div> <!-- /.row -->
   <?php } ?>
 
+  <div class="main-cta">
+    <div class="row">
+      <div class="cta-inner">
+        <p>does this sound like</p>
+        <p class="text-right"><strong>what you need?</strong></p>
+      </div>
+
+      <div class="cta-button">
+        <a href="/contact/work-with-us/" title="CTA Button" class="button">Let's Get In Touch!</a>
+      </div>
+    </div>
+  </div>
+
 <?php $this->tplDisplay("inc_footer.php"); ?>
