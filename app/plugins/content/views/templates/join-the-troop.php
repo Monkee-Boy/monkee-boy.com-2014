5d1c566b121c83d2ac912cfa4a24ede8689acbc5
James Fleeting <hello@jamesfleeting.com>
2015-03-11 10:28:08 -0500

    Adding join the troop content template.

---
+++ app/plugins/content/views/templates/join-the-troop.php
@@ -0,0 +1,28 @@
+<!--
+@Name: Join The Troop
+@Description: Content template for job postings includes the form.
+@Version: 1.0
+@Restricted: true
+@Author: Monkee-Boy
+-->
+
+<?php $this->tplDisplay("inc_header.php", ['menu'=>'join-the-troop', 'sPageTitle'=>$sTitle, 'seo_title'=>$aContent['seo_title'], 'seo_description'=>$aContent['seo_description'], 'seo_keywords'=>$aContent['seo_keywords']]); ?>
+
+  <div class="row page-title">
+    <h1><?= $aContent['title']; ?></h1>
+    <p class="subtitle"><?= $aContent['subtitle']; ?></p>
+  </div>
+
+  <div class="row">
+    <div class="single-column content-block">
+      <?= $aContent['content']; ?>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="full">
+      [form here]
+    </div>
+  </div>
+
+<?php $this->tplDisplay("inc_footer.php"); ?>
