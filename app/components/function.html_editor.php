6df373eac4b423a1c5cb26750c4c87aa7dc5ceb8
John Hoover <defvayne23@gmail.com>
2015-03-11 14:47:23 -0500

    Allow script and iframe tags in editor.
    Since we upgraded tinyMCE, we lost the filemanager. Added a free filemanager.

--- app/components/function.html_editor.php
+++ app/components/function.html_editor.php
@@ -11,32 +11,36 @@ function html_editor($content = '', $name = '', $label = '', $class = '', $theme
 	$return = "{footer}\n";
 	$return .= "<script>if(typeof(tinymce) === 'undefined') { document.write('<script src=\"/js/admin/tiny_mce/jquery.tinymce.min.js\"><\/script><script src=\"/js/admin/tiny_mce/tinymce.min.js\"><\/script>'); }</script>\n";
 	$return .= "<script>\n";
-	$return .= "tinyMCE.init({\n";
-
-	if($_COOKIE[$name."_editor"] == "html")
-		$return .= "\tmode : 'none',\n";
-	else
-		$return .= "\tmode : 'textareas',\n";
-
+	$return .= "tinymce.init({\n";
+	$return .= "\tmode : 'textareas',\n";
 	$return .= "\ttheme : 'modern',\n";
-	$return .= "\tplugins: ['advlist autolink autosave link image lists charmap print preview hr anchor pagebreak spellchecker', 'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking', 'table contextmenu directionality emoticons template textcolor paste fullpage textcolor colorpicker textpattern'],\n";
-	$return .= "\tmenubar: false,\n";
+	$return .= "\tplugins: ['advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker', 'searchreplace wordcount visualblocks visualchars code fullscreen media nonbreaking', 'table contextmenu directionality textcolor paste textcolor colorpicker textpattern'],\n";
   $return .= "\ttoolbar_items_size: 'small',\n";
 	$return .= "\teditor_selector : '".$name."_editor',\n";
 	$return .= "\twidth: '".$width."',\n";
 	$return .= "\theight: '".$height."',\n";
+	// $return .= "\tconvert_urls: false,\n";
+	$return .= "\trelative_urls: false,\n";
 
 	if($theme == "simple") {
 		// $return .= "\ttheme_advanced_buttons1 : 'pastetext,pasteword,|,bold,italic,underline,strikethrough,|,numlist,bullist,|,link,unlink,|,undo,redo',\n";
 		// $return .= "\ttheme_advanced_buttons2 : '',\n";
 		// $return .= "\ttheme_advanced_buttons3 : '',\n";
 	} else {
-		$return .= "\ttoolbar1: 'newdocument fullpage | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | styleselect formatselect fontselect fontsizeselect',
-        toolbar2: 'cut copy paste | searchreplace | bullist numlist | outdent indent blockquote | undo redo | link unlink anchor image media code | insertdatetime preview | forecolor backcolor',
-        toolbar3: 'table | hr removeformat | subscript superscript | charmap emoticons | print fullscreen | ltr rtl | spellchecker | visualchars visualblocks nonbreaking template pagebreak restoredraft',
+		$return .= "\ttoolbar1: 'formatselect | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent blockquote',
 \n";
+		$return .= "\tmenu : { // this is the complete default configuration
+        edit   : {title : 'Edit'  , items : 'undo redo | cut copy paste pastetext | selectall | searchreplace'},
+        insert : {title : 'Insert', items : 'link unlink anchor image media | hr'},
+        format : {title : 'Format', items : 'bold italic underline strikethrough superscript subscript | fontsizeselect formats | removeformat'},
+        table  : {title : 'Table' , items : 'inserttable tableprops deletetable | cell row column'},
+        tools  : {title : 'Tools' , items : 'code'}
+    },";
 	}
-	$return .= "\tvalid_elements : '*[*]',\n";
+	$return .= "\textended_valid_elements : 'pre,script[src],code,iframe[width|height|src|scrolling]',\n";
+	$return .= "\texternal_filemanager_path: '/js/admin/tiny_mce/plugins/filemanager/',\n";
+	$return .= "\tfilemanager_title: 'Filemanager',\n";
+	$return .= "\texternal_plugins: { 'filemanager': '/js/admin/tiny_mce/plugins/filemanager/plugin.min.js'},\n";
 	$return .= "});\n";
 	$return .= "</script>\n";
 	$return .= "{/footer}\n";
