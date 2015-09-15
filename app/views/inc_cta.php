f2143817627dd4339f2227fa49bb0edb24a0ef75
David Saunders <david@monkee-boy.com>
2015-09-15 15:41:23 -0500

    Improved form styling; Added label animation when input is active

--- app/views/inc_cta.php
+++ app/views/inc_cta.php
@@ -23,10 +23,18 @@ if(isset($service) && !empty($service)) {
     <div class="cta-inner newsletter-inner">
       <p>Raise Your Digital IQ</p>
       <p>Get FREE advice every month from our troop of experts.</p>
-      <form class="request-newsletter-form" name="request-newsletter" action="index.php" method="post">
-        <input type="text" name="name" placeholder="NAME">
-        <input type="text" name="email" placeholder="EMAIL ADDRESS">
-        <button type="submit" class="submit">Join Now!</button>
+      <form class="request-newsletter-form" name="request-newsletter" action="/" method="post">
+        <span class="input-wrapper one-third">
+          <input type="text" name="name" required>
+          <label for="name">Name</label>
+        </span>
+        <span class="input-wrapper one-third">
+          <input type="text" name="email" required>
+          <label for="email">Email Address</label>
+        </span>
+        <span class="input-wrapper one-third">
+          <button type="submit" class="submit">Join Now!</button>
+        </span>
       </form>
       <p>(Monkees don't spam. We won't ever share your email address)</p>
     </div>
